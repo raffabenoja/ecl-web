f917f5c4e31b84ee594fd5760009316f122f1b53
Nam Le <namle117@gmail.com>
2020-03-03 14:51:23 +0800

    Updated `<Footer />` styling Added `device.js` for @media CSS detection

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,33 +5,38 @@ import React from "react"
 import styled from "styled-components"
 import { Container, Row, Col } from "reactstrap"
 
+import { device } from "utils"
+
+// TODO: Added href links
 const Footer = ({ siteTitle }) => (
   <Background>
     <Container>
       <Row>
-        <Col>
-          844 Dubuque Ave
-          <br /> South San Francisco, CA 94080
+        <Col sm={12} md={4} lg={4}>
+          <Address>
+            844 Dubuque Ave
+            <br /> South San Francisco, CA 94080
+          </Address>
         </Col>
-        <Col>
-          HOME
+        <Col sm={12} md={4} lg={4}>
+          <a href="#">HOME</a>
           <br />
-          HOW THE ECL WORKS
+          <a href="#">HOW THE ECL WORKS</a>
           <br />
-          EXPERIMENTAL CAPABILITIES
+          <a href="#">EXPERIMENTAL CAPABILITIES</a>
           <br />
-          ABOUT US
+          <a href="#">ABOUT US</a>
         </Col>
-        <Col>
-          CAREERS
+        <Col sm={12} md={4} lg={4}>
+          <a href="#">CAREERS</a>
           <br />
-          LEGAL
+          <a href="#">LEGAL</a>
           <br />
-          CONTACT US
+          <a href="#">CONTACT US</a>
         </Col>
       </Row>
-      <Row style={{ marginTop: "50px" }}>
-        <Col>
+      <Row style={{ marginTop: "55px" }}>
+        <Col sm={12} md={12} lg={12}>
           <Copyright>
             © {new Date().getFullYear()} Emerald Cloud Lab, Inc.
           </Copyright>
@@ -54,6 +59,35 @@ export default Footer
 const Background = styled.div`
   background-color: #16191c;
   color: #7b8690;
-  padding: 40px 50px;
+  padding: 55px 10px 40px 10px;
+
+  @media ${device.lg} {
+    padding: 40px 50px;
+  }
+
+  a {
+    color: rgb(154, 154, 154);
+    font-size: 14px;
+    line-height: 30px;
+    font-weight: 500;
+
+    &:hover {
+      color: rgba(177, 186, 194, 0.6);
+      text-decoration: none;
+    }
+  }
 `
-const Copyright = styled.span``
+const Address = styled.address`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 22px;
+  margin-bottom: 50px;
+`
+
+const Copyright = styled.span`
+  font-family: "Ringside Regular";
+  font-size: 12px;
+  font-weight: 300;
+  color: #76828c;
+  margin-top: 55px;
+`
---
+++ src/utils/device.js
@@ -0,0 +1,13 @@
+const size = {
+  sm: "480px",
+  md: "768px",
+  lg: "1024px",
+}
+
+export const device = {
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+}
+
+export default device
